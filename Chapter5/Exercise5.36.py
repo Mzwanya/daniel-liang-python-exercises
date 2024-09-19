8eb020c448c556a6b9f9d724a2966df118d9e252
Dennis Kaunda <mphatso.myprojects@gmail.com>
2024-09-19 02:03:03 +0200

    add  solutions

--- Chapter5/Exercise5.36.py
+++ Chapter5/Exercise5.36.py
@@ -1,62 +1,35 @@
 # Game: scissor, rock, paper
 
 import random
-import sys
 
-count = 0
-while count <= 2 or count <= -2:
+user_score = 0
+computer_score = 0
 
-    # Generate scissor, rock, or paper
-    computer_id = random.randint(0, 2)
+# Play the game until either the user or the computer wins more than two time
+while user_score <= 2 and computer_score <= 2:
+    # Computer randomly chooses a number between 0 and 2
+    computer_input = random.randint(0, 2)
 
-    # Prompt the user to enter scissor, rock, or paper
-    player_id = int(input("scissor (0), rock (1), paper (2): "))
+     # Ask user to input their choice
+    user_input = int(input("scissor (0), rock (1), paper (2): "))
 
-    computer = ''
-    player = ''
-
-    if computer_id == 0:
-        computer = 'scissor'
-    elif computer_id == 1:
-        computer = 'rock'
+    # Determine the winner based on the rules of the game
+    if user_input == computer_input:
+        print("It's a draw!\n")
+    elif (user_input == 0 and computer_input == 2) or \
+         (user_input == 1 and computer_input == 0) or \
+         (user_input == 2 and computer_input == 1):
+        print("You win this round!\n")
+        user_score += 1
     else:
-        computer = 'paper'
+        print("Computer wins this round!\n")
+        computer_score += 1
+    
+    # Display the current scores
+    print(f"Current Scores - You: {user_score}, Computer: {computer_score}\n")
 
-    if player_id == 0:
-        player = 'scissor'
-    elif player_id == 1:
-        player = 'rock'
-    elif player_id == 2:
-        player = 'paper'
-    else:
-        sys.exit("Error: invalid input!")
-
-    # Check the guess
-    if player == computer:
-        print(f"The computer is {computer}. you are {player} too. It is a draw.")
-    elif player == 'rock':
-        if computer == 'scissor':
-            print("The computer is scissor. you are rock. You won.")
-            count += 1
-        else:             # computer == 'paper'
-            print("The computer is paper. you are rock. You lose.")
-            count -= 1
-    elif player == 'scissor':
-        if computer == 'rock':
-            print("The computer is rock. you are scissor. You lose.")
-            count -= 1
-        else:             # computer == 'paper'
-            print("The computer is paper. you are scissor. You won.")
-            count += 1
-    else:                 # user == 'paper'
-        if computer == 'rock':
-            print("The computer is rock. you are paper. You won.")
-            count += 1
-        else:             # computer == 'scissor'
-            print("The computer is scissor. you are paper. You lose.")
-            count -= 1
-
-if count > 2:
-    print("You won more than two times!")
+# Final result
+if user_score > 2:
+    print("Congratulations! You won the game!")
 else:
-    print("The computer won more than two times!")
+    print("Computer won the game! Better luck next time!")
