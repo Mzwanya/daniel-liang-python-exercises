b64e51d8eb0aa064dd1647999eb80223c05caf09
Dennis Kaunda <mphatso.myprojects@gmail.com>
2024-09-20 18:49:28 +0200

    add soloutions

---
+++ Chapter6/Exercise6.01.py
@@ -0,0 +1,17 @@
+def get_pentagonal_number(n):
+    number = n * (3 * n - 1) / 2
+    return int(number)
+
+
+def main():
+    count = 1
+    for i in range(1, 101):
+        print(get_pentagonal_number(i), end=' ')
+        if count % 10 == 0:
+            print()
+            count = 0
+
+        count += 1
+
+
+main()
