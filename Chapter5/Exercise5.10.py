aa722b23ba97420b800ae5ffa90fd0c72622e65b
Dennis Kaunda <mphatso.myprojects@gmail.com>
2024-09-24 21:34:29 +0200

    update files

--- Chapter5/Exercise5.10.py
+++ Chapter5/Exercise5.10.py
@@ -1,9 +1,8 @@
 number_of_students = int(input("Enter the number of students: "))
+
 count = 0
-score = 0
-student = ''
-largest_student = ''
 largest_score = 0
+
 while count < number_of_students:
     student = input("Student: ")
     score = eval(input("Score: "))
@@ -11,4 +10,5 @@ while count < number_of_students:
         largest_student = student
         largest_score = score
     count += 1
+    
 print("The student with highest score is",largest_student, "and the score is", largest_score)
